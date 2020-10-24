c0204df375d48392758afabae6417424e53af0c6
MianNouman <nouman.ghaffar@careem.com>
2020-10-24 17:17:37 +0500

    Dependency Injection and basic classes are added

---
+++ app/src/main/java/com/nouman/gittreadingrepo/di/viewmodeldi/ViewModelKey.kt
@@ -0,0 +1,14 @@
+package com.nouman.gittreadingrepo.di.viewmodeldi
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+/**
+ * Key to be used for ViewModel
+ */
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
